d5356b0ec3d77ee0ebf2bc5cd2cb652046bcc24e
goobercoder <vaino.o.hanninen@gmail.com>
2024-10-02 17:57:37 +0300

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,51 @@
+let taustakuva;
+let ankkakuva;
+let leveys;
+let korkeus;
+let ankka1;
+let ankka2;
+
+
+// Load the image and create a p5.Image object.
+function preload() {
+  taustakuva = loadImage('/images/lampi.png');
+  ankkakuva = loadImage('/images/ankka.png');
+}
+
+
+
+function setup() {
+  leveys = windowWidth;
+  korkeus = windowWidth/3;
+    createCanvas(leveys, korkeus);
+    
+    ankka1 = new Ankka();
+    ankka2 = new Ankka();
+  }
+  
+  function draw() {
+  leveys = windowWidth;
+  korkeus = windowWidth/3;
+  image(taustakuva, 0, 0, leveys, korkeus);
+  ankka1.liikuta();
+  ankka2.liikuta();
+  }
+
+  function windowResized(){
+  leveys = windowWidth;
+  korkeus = windowWidth/3;
+  resizeCanvas(leveys, korkeus);
+  }
+
+  class Ankka {
+    constructor() {
+      this.X = 0;
+      this.X_speed = random(1,10);
+    }
+    
+    liikuta(){
+      this.X = this.X + this.X_speed;
+      image(ankkakuva, this.X, korkeus/2, 150, 150)
+    }
+
+  }
