a7f98a33793e55b89837b22a02c110b4a627b991
goobercoder <vaino.o.hanninen@gmail.com>
2024-10-30 16:13:50 +0000

    modified:   public/script.js

--- public/script.js
+++ public/script.js
@@ -2,14 +2,13 @@ let taustakuva;
 let ankkakuva;
 let leveys;
 let korkeus;
-let ankka1;
-let ankka2;
-let lautanleveys;
-let lautankorkeus;
-let lauty;
+let lautanLeveys;
+let lautanKorkeus;
+let lautanY;
 let omgGravitaatio = 0.05;
 let laavaY;
 let lautanX;
+let Elävienlista = [];
 
 // Load the image and create a p5.Image object.
 function preload() {
@@ -22,23 +21,24 @@ function preload() {
 function setup() {
   leveys = windowWidth;
   korkeus = windowWidth/2;
-    createCanvas(leveys, korkeus);
-    lautanleveys = leveys/5;
-    lautankorkeus = korkeus/17;
-    lauty = korkeus * 0.93;
-    laavaY = korkeus * 0.9;
-    ankka1 = new Ankka();
-    ankka2 = new Ankka();
+  lautanleveys = leveys/5;
+  lautankorkeus = korkeus/17;
+  lautanY = korkeus * 0.93;
+  laavaY = korkeus * 0.9;
+  createCanvas(leveys, korkeus); 
+  LuoAnkkoja();
+ 
   }
   
   function draw() {
   leveys = windowWidth;
   korkeus = windowWidth/2;
   image(taustakuva, 0, 0, leveys, korkeus);
-  ankka1.liikuta();
-  ankka2.liikuta();
   luoJaliikutaLauttaa();
   LuoLaava();
+  Elävienlista.forEach(function(ankkaDoge, monesko){
+    ankkaDoge.liikuta()
+  });
   }
 
   function windowResized(){
@@ -49,41 +49,49 @@ function setup() {
   
   function LuoLaava(){
     fill("#cf1020 ")
-    rect(0, laavaY + 50, leveys, lautankorkeus - 10);
+    rect(-10, laavaY + 70, leveys, lautankorkeus - 10);
   }
 
   function luoJaliikutaLauttaa(){
     fill("Chocolate")
-    rect(mouseX, lauty, lautanleveys, lautankorkeus, 360);
+    rect(mouseX, lautanY, lautanleveys, lautankorkeus, 360);
   }
+
+function LuoAnkkoja(){
+  ankkaDoge = new Ankka();
+  Elävienlista.unshift(ankkaDoge);
+  setTimeout(LuoAnkkoja, 3000)
+  console.log(Elävienlista)
+
+}
 
   class Ankka {
     constructor() {
       this.X = 0;
       this.Y = korkeus/2;
       this.xSpeed = random(1,5);
-      this.ySpeed = -3;
+      this.ySpeed = -5;
       this.size = lautanleveys / 1,9;
     }
     
     liikuta(){
-      lautanleveys = leveys/5;
-      lautankorkeus = korkeus/17;
-      lauty = korkeus * 0.93;
+      lautanKorkeus = korkeus/10;
+      lautanLeveys = leveys/10;
+      lautanY = korkeus * 0.9;
       lautanX = mouseX;
       this.X = this.X + this.xSpeed;
       this.ySpeed = this.ySpeed + omgGravitaatio;
 
-      if( this.X > lautanX && this.X < lautanX + lautanleveys );
-      {
-        if(this.Y + this.korkeus > lauty && this.Y < lauty + lautankorkeus);
-        {
-          this.ySpeed = -abs(this.ySpeed);
-        }
-      }
-        
-        
 
+
+      if( this.X > lautanX && this.X < lautanX + lautanLeveys )
+      {
+        //otettu pois this.Y jälkeen this.korkeus niin alkoi toimimaan
+        if(this.Y  + lautanKorkeus*2> lautanY && this.Y < lautanY + lautanKorkeus*2)
+          {
+            this.ySpeed = -abs(this.ySpeed);
+          }
+      }
       this.Y = this.Y + this.ySpeed;
       image(ankkakuva, this.X, this.Y, this.size, this.size)
     }
